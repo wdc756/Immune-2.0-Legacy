2a255f05c7e91cacf0252b2f4e5c5a94aa7d15bb
wdc756 <wdc23a@acu.edu>
2024-10-17 17:47:34 -0500

    Updated the way Object Pools are handled in VisualManager
    I used a hybrid approach, using both static references to prefabs and a dynamic list of the actual Object Pools being used

--- Assets/Scripts/Visuals/VisualManager.cs
+++ Assets/Scripts/Visuals/VisualManager.cs
@@ -20,26 +20,29 @@ public class VisualManager : MonoBehaviour
     [SerializeField, Tooltip("Used to reference all the VisualScenes")]
     private List<VisualScene> visualSceneList = new List<VisualScene>();
 
+    [SerializeField, Tooltip("Used to group the ObjectPoolingHelpers together for organization")]
+    private GameObject objectPoolingHelpersParent;
     [SerializeField, Tooltip("Prefab used to generate Object Pooling Helpers")]
     private ObjectPoolingHelper objectPoolingHelperPrefab;
     //This is a list because then we can add/subtract stuff as needed during development or for other gamemodes
     [SerializeField, Tooltip("Stores all the ObjectPoolingHelpers used throughout the game")]
     private List<ObjectPoolingHelper> objectPoolingHelperList = new List<ObjectPoolingHelper>();
 
-    //[SerializeField, Tooltip("Reference to ObjectPoolingHelper for civilianCells")]
-    //private ObjectPoolingHelper civilianPool;
-    //[SerializeField, Tooltip("Reference to ObjectPoolingHelper for macrophages")]
-    //private ObjectPoolingHelper macrophagePool;
-    //[SerializeField, Tooltip("Reference to ObjectPoolingHelper for neutrophiles")]
-    //private ObjectPoolingHelper neutrophilePool;
-    //[SerializeField, Tooltip("Reference to ObjectPoolingHelper for TCells")]
-    //private ObjectPoolingHelper TCellPool;
-    //[SerializeField, Tooltip("Reference to ObjectPoolingHelper for BCells")]
-    //private ObjectPoolingHelper BCellPool;
-    //[SerializeField, Tooltip("Reference to ObjectPoolingHelper for bacteria")]
-    //private ObjectPoolingHelper bacteriaPool;
-    //[SerializeField, Tooltip("Reference to ObjectPoolingHelper for infected Cells")]
-    //private ObjectPoolingHelper infectedCellPool;
+    //these are here to help us instantiate the relevant ObjectPoolingHelpers easier
+    [SerializeField, Tooltip("Reference to ObjectPoolingHelper for Prefab civilianCells")]
+    private GameObject civilianPoolPre;
+    [SerializeField, Tooltip("Reference to ObjectPoolingHelper for Prefab macrophages")]
+    private GameObject macrophagePoolPre;
+    [SerializeField, Tooltip("Reference to ObjectPoolingHelper for Prefab neutrophiles")]
+    private GameObject neutrophilePoolPre;
+    [SerializeField, Tooltip("Reference to ObjectPoolingHelper for Prefab TCells")]
+    private GameObject TCellPoolPre;
+    [SerializeField, Tooltip("Reference to ObjectPoolingHelper for Prefab BCells")]
+    private GameObject BCellPoolPre;
+    [SerializeField, Tooltip("Reference to ObjectPoolingHelper for Prefab bacteria")]
+    private GameObject bacteriaPoolPre;
+    [SerializeField, Tooltip("Reference to ObjectPoolingHelper for Prefab infected Cells")]
+    private GameObject infectedCellPoolPre;
 
     [SerializeField, Tooltip("Reference to the SimulationManager for data; DO NOT SET ON COMPILE TIME, this will be set during runtime")]
     private SimulationManager simulationManager;
@@ -65,12 +68,59 @@ public class VisualManager : MonoBehaviour
     //Generates scenes using the visualScenePrefab
     private void InstantiateVisualScenes()
     {
-
+        VisualScene civilianCells = new VisualScene();
+        //civilianCells.
+    }
+    //checks if all the object pooling helper prefabs are not null
+    private void CheckObjectPoolingHelperPrefabs()
+    {
+        if (civilianPoolPre == null)
+        {
+            Debug.LogWarning("civilianPoolPre is null.");
+        }
+        if (macrophagePoolPre == null)
+        {
+            Debug.LogWarning("macrophagePoolPre is null.");
+        }
+        if (neutrophilePoolPre == null)
+        {
+            Debug.LogWarning("neutrophilePoolPre is null.");
+        }
+        if (TCellPoolPre == null)
+        {
+            Debug.LogWarning("TCellPoolPre is null.");
+        }
+        if (BCellPoolPre == null)
+        {
+            Debug.LogWarning("BCellPoolPre is null.");
+        }
+        if (bacteriaPoolPre == null)
+        {
+            Debug.LogWarning("bacteriaPoolPre is null.");
+        }
+        if (infectedCellPoolPre == null)
+        {
+            Debug.LogWarning("infectedCellPoolPre is null.");
+        }
     }
     //Generates objectpools using the objectPoolingHelperPrefab
     private void InstantiateObjectPoolingHelpers()
     {
+        ObjectPoolingHelper civilians = Instantiate(civilianPoolPre, objectPoolingHelpersParent.transform).GetComponent<ObjectPoolingHelper>();
+        objectPoolingHelperList.Add(civilians);
 
+        ObjectPoolingHelper macrophages = Instantiate(macrophagePoolPre, objectPoolingHelpersParent.transform).GetComponent<ObjectPoolingHelper>();
+        objectPoolingHelperList.Add(macrophages);
+        ObjectPoolingHelper neutrophils = Instantiate(neutrophilePoolPre, objectPoolingHelpersParent.transform).GetComponent<ObjectPoolingHelper>();
+        objectPoolingHelperList.Add(neutrophils);
+        ObjectPoolingHelper TCells = Instantiate(TCellPoolPre, objectPoolingHelpersParent.transform).GetComponent<ObjectPoolingHelper>();
+        objectPoolingHelperList.Add(TCells);
+        ObjectPoolingHelper BCells = Instantiate(BCellPoolPre, objectPoolingHelpersParent.transform).GetComponent<ObjectPoolingHelper>();
+        objectPoolingHelperList.Add(BCells);
+        ObjectPoolingHelper bacteria = Instantiate(bacteriaPoolPre, objectPoolingHelpersParent.transform).GetComponent<ObjectPoolingHelper>();
+        objectPoolingHelperList.Add(bacteria);
+        ObjectPoolingHelper infectedCells = Instantiate(infectedCellPoolPre, objectPoolingHelpersParent.transform).GetComponent<ObjectPoolingHelper>();
+        objectPoolingHelperList.Add(infectedCells);
     }
     //returns whether or not the level is ready, and will output why the level is not ready
     bool isLevelReady()
@@ -84,6 +134,12 @@ public class VisualManager : MonoBehaviour
         if (simulationManager == null)
         {
             Debug.LogWarning("simulationManager is null on VisualManager");
+            return false;
+        }
+
+        if (objectPoolingHelpersParent == null)
+        {
+            Debug.LogWarning("objectPoolingHelpersParent is null on VisualManager");
             return false;
         }
 
